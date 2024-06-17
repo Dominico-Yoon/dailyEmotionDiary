e0c26f0739b61d75a8e32146d71ab00ef311177f
Dominico Yoon <808dominico@gmail.com>
2024-06-17 20:43:38 +0900

    20240617_2041 1. Edit Components 수정 * 확장자 .jsx에서 .tsx로 수정 * context사용 시, null 타입이 들어올 수 있기 때문에 커스텀 훅 만들어서 사용 * params.id는 string 타입을 갖고 있기 때문에 커스텀 훅인 useDiary에서 사용 불가 -> 조건식을 추가하여 number 타입이 되게끔 수정

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -2,34 +2,39 @@ import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import { useContext } from "react";
-import { DiaryDispatchContext } from "../App";
+import { useDiaryDispatch } from "../App";
 import Editor from "../components/Editor";
 import useDiary from "../hooks/useDiary";
 import usePageTitle from "../hooks/usePageTitle";
+import { DiaryType } from "../types";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
-  const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
+  // const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
+  const dispatch = useDiaryDispatch();
   usePageTitle(`${params.id}번 일기 수정`);
 
-  const curDiaryItem = useDiary(params.id);
+  // params.id를 number로 변환하고, 변환 실패 시 undefined를 반환
+  const id = params.id ? Number(params.id) : undefined;
 
-  const onSubmit = (input) => {
+  // id가 undefined인 경우 예외 처리
+  if (id === undefined) {
+    return <div>잘못된 접근입니다.</div>;
+  }
+
+  const curDiaryItem = useDiary(id);
+
+  const onSubmit = (input: DiaryType) => {
     if (window.confirm("일기를 정말 수정할까요?")) {
-      onUpdate(
-        params.id,
-        input.createdDate.getTime(),
-        input.emotionId,
-        input.content
-      );
+      dispatch.onUpdate(id, input.createdDate, input.emotionId, input.content);
     }
     nav("/", { replace: true });
   };
 
   const onClickDelete = () => {
     if (window.confirm("정말 삭제 하시겠습니까?")) {
-      onDelete(params.id);
+      dispatch.onDelete(id);
       nav("/", { replace: true });
     }
   };
